af861bc4d5961222daabaea5a38a871797543813
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2025-10-12 19:43:04 +0530

    Remove data

--- aws/eks/velero.tf
+++ aws/eks/velero.tf
@@ -37,7 +37,7 @@ resource "aws_iam_role" "velero" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:velero:velero"
+            "${replace(aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:velero:velero"
           }
         }
       }
