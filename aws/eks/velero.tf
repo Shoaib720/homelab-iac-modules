f5cd51ab8664ae9d81e0427e92a3e30415b3053f
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2025-10-12 21:54:43 +0530

    Renamed velero sa

--- aws/eks/velero.tf
+++ aws/eks/velero.tf
@@ -37,7 +37,7 @@ resource "aws_iam_role" "velero" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "${replace(aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:velero:homelab-velero-server"
+            "${replace(aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:velero:velero"
           }
         }
       }
