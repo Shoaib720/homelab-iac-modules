58380f1e4c53aff90fb63404b24ae1f2ea878d13
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2025-10-18 14:17:21 +0530

    Reverted

--- aws/eks/velero.tf
+++ aws/eks/velero.tf
@@ -24,28 +24,28 @@ resource "aws_iam_policy" "velero" {
   })
 }
 
-# resource "aws_iam_role" "velero" {
-#   name = "velero-irsa-role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Principal = {
-#           Federated = aws_iam_openid_connect_provider.oidc_provider.arn
-#         }
-#         Action = "sts:AssumeRoleWithWebIdentity"
-#         Condition = {
-#           StringEquals = {
-#             "${replace(aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:velero:velero"
-#           }
-#         }
-#       }
-#     ]
-#   })
-# }
+resource "aws_iam_role" "velero" {
+  name = "velero-irsa-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.oidc_provider.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:velero:velero"
+          }
+        }
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "velero_attach" {
-#   role       = aws_iam_role.velero.name
-#   policy_arn = aws_iam_policy.velero.arn
-# }
+resource "aws_iam_role_policy_attachment" "velero_attach" {
+  role       = aws_iam_role.velero.name
+  policy_arn = aws_iam_policy.velero.arn
+}
