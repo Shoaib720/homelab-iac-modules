75d10ba3eed1aad1725b67adc740160b612a6f50
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2025-10-18 14:00:33 +0530

    Disable oidc to delete

--- aws/eks/control_plane.tf
+++ aws/eks/control_plane.tf
@@ -47,8 +47,8 @@ resource "aws_eks_cluster" "cluster" {
   ]
 }
 
-resource "aws_iam_openid_connect_provider" "oidc_provider" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks_tls_cert.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
-}
+# resource "aws_iam_openid_connect_provider" "oidc_provider" {
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = [data.tls_certificate.eks_tls_cert.certificates[0].sha1_fingerprint]
+#   url             = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+# }
