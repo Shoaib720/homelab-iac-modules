3f7c05d8acf7396cd7cdb7c85614dc9a07340464
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2025-10-19 00:42:45 +0530

    Fix name

--- aws/eks/csi_snapshot_controller_addon.tf
+++ aws/eks/csi_snapshot_controller_addon.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_addon" "ebs_snapshot_controller" {
   cluster_name      = aws_eks_cluster.cluster.name
-  addon_name        = "csi-snapshot-controller"
+  addon_name        = "snapshot-controller"
   addon_version     = "v8.3.0-eksbuild.1"
   resolve_conflicts_on_update = "OVERWRITE"
 
