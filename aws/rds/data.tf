c28ae72462f81d754e6b3fc336ca338944b60afb
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2025-10-19 13:35:07 +0530

    Fix incorrect path

--- aws/rds/data.tf
+++ aws/rds/data.tf
@@ -1,5 +1,5 @@
 data "external" "rds_snapshot_exists" {
-  program = ["bash", "${path.module}/rds/check_snapshot.sh", "${var.rds_instance_name}"]
+  program = ["bash", "${path.module}/check_snapshot.sh", "${var.rds_instance_name}"]
 }
 
 data "aws_db_snapshot" "latest_snapshot" {
