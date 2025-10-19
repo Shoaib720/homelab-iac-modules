fe2f74f7c9c31318c8b15be54aa2e2f7e2502401
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2025-10-19 13:33:33 +0530

    Fix snapshot issue

--- aws/rds/data.tf
+++ aws/rds/data.tf
@@ -1,5 +1,9 @@
+data "external" "rds_snapshot_exists" {
+  program = ["bash", "${path.module}/rds/check_snapshot.sh", "${var.rds_instance_name}"]
+}
+
 data "aws_db_snapshot" "latest_snapshot" {
-  count                  = local.snapshot_id != "" ? 1 : 0
+  count                  = data.external.rds_snapshot_exists.result["exists"] == "true" ? 1 : 0
   db_instance_identifier = var.rds_instance_name
   most_recent            = true
 }
